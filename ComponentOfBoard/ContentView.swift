1af6de8c37bc6d1e386d3a24299f8f1f689df598
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-06 17:55:40 +0500

    The End

--- ComponentOfBoard/ContentView.swift
+++ ComponentOfBoard/ContentView.swift
@@ -8,46 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sliderValue = 100.0
+    @State private var sliderValue = Double(Int.random(in: 1...255))
     @State private var userName = ""
     @State private var displayName = ""
     @State private var isPresented = false
-    
     
     var body: some View {
         ZStack {
             Color(.init(srgbRed: 0.1, green: 0.3, blue: 0.5, alpha: 1))
                 .ignoresSafeArea()
             VStack(spacing: 40) {
-                Image(systemName: "swift")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-                    .foregroundStyle(.tint)
-                
-                Text(sliderValue.formatted())
-                    .frame(width: 300, height: 40)
-                    .background(.blue, in: .buttonBorder)
-                    .foregroundStyle(.white)
-                    .font(.system(.title, design: .rounded, weight: .heavy))
-                
-                Text(displayName)
-                    .frame(width: 300, height: 40)
-                    .font(.system(.title, design: .rounded, weight: .heavy))
-                    .foregroundStyle(.white)
-                    .overlay(
-                        RoundedRectangle(
-                            cornerRadius: 10
-                        ).stroke(.white, lineWidth: 1)
-                    )
-                
-                Slider(
-                    value: $sliderValue,
-                    in: 1...255,
-                    step: 1
-                )
+                ImageView()
+                TextSliderView(sliderValue: $sliderValue)
+                TextFieldView(displayName: $displayName)
+                SliderView(sliderValue: $sliderValue)
                 TextField("Enter yor name", text: $userName)
                     .textFieldStyle(.roundedBorder)
-                
                 Button(action: getText) {
                     Text("Done")
                         .font(.title)
@@ -68,7 +44,6 @@ struct ContentView: View {
         }
     }
     
-    
     private func getText() {
         if let _ = Double(userName) {
             isPresented.toggle()
@@ -82,4 +57,54 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ImageView: View {
+    var body: some View {
+        Image(systemName: "swift")
+            .resizable()
+            .frame(width: 50, height: 50)
+            .foregroundStyle(.tint)
+    }
+}
+
+struct SliderView: View {
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        Slider(
+            value: $sliderValue,
+            in: 1...255,
+            step: 1
+        )
+    }
+}
+
+struct TextSliderView: View {
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        Text(lround(sliderValue).formatted())
+            .frame(width: 300, height: 40)
+            .font(.system(.title, design: .rounded, weight: .heavy))
+            .background(.blue, in: .buttonBorder)
+            .foregroundStyle(.white)
+    }
+}
+
+struct TextFieldView: View {
+    @Binding var displayName: String
+    
+    var body: some View {
+        Text(displayName)
+            .frame(width: 300, height: 40)
+            .font(.system(.title, design: .rounded, weight: .heavy))
+        
+            .foregroundStyle(.white)
+            .overlay(
+                RoundedRectangle(
+                    cornerRadius: 10
+                ).stroke(.white, lineWidth: 1)
+            )
+    }
 }
