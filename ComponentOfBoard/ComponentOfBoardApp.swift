d3cf7d6b27b620207b12416cfc4795e78edd9426
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-06 10:34:18 +0500

    Initial Commit

---
+++ ComponentOfBoard/ComponentOfBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentOfBoardApp.swift
+//  ComponentOfBoard
+//
+//  Created by Dmitry Parhomenko on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ComponentOfBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
