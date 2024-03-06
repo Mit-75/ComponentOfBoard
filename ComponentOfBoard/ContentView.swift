3f7ff9f05ecf7144af5a45035f0ca609fc3d4808
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-06 13:04:09 +0500

    - Расположил элементы на экране

--- ComponentOfBoard/ContentView.swift
+++ ComponentOfBoard/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue = 100.0
+    @State private var userName = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.init(srgbRed: 0.1, green: 0.3, blue: 0.5, alpha: 1))
+                .ignoresSafeArea()
+            VStack(spacing: 40) {
+                Image(systemName: "swift")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .foregroundStyle(.tint)
+                
+                Text(sliderValue.formatted())
+                    .frame(width: 150, height: 40)
+                    .background(.blue, in: .buttonBorder)
+                    .foregroundStyle(.white)
+                    .font(.system(.title, design: .rounded, weight: .heavy))
+                Text("№1")
+                    .frame(width: 150, height: 40)
+                    .font(.system(.title, design: .rounded, weight: .heavy))
+                    .foregroundStyle(.white)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(.white, lineWidth: 1))
+                Slider(
+                    value: $sliderValue,
+                    in: 1...255,
+                    step: 1
+                )
+                TextField("Enter yor name", text: $userName)
+                    .textFieldStyle(.roundedBorder)
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Done")
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .foregroundStyle(.white)
+                }
+                .frame(minWidth: 150, minHeight: 60)
+                .background(.gray)
+                .clipShape(.rect(cornerRadius: 10))
+                
+                Spacer()
+            }
+            .padding(20)
         }
-        .padding()
     }
 }
 
